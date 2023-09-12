02a93fa1178a4a1633633741e0229d3a84239ff2
Issen <issen.zhang@outlook.com>
2023-09-12 17:12:46 +0800

    0912

---
+++ HELPER.lua
@@ -0,0 +1,161 @@
+HELPER = {}
+
+HELPER.SoundFileName = "radio click.ogg"
+
+-- 具体参照ENUMS.ReportingName
+HELPER.ReportingName = -- 待继续补充
+{
+    -- Fighters
+    Dragon = "JF-17", -- China, correctly Fierce Dragon, Thunder for PAC
+    Fagot = "MiG-15",
+    Farmer = "MiG-19", -- Shenyang J-6 and Mikoyan-Gurevich MiG-19
+    Felon = "Su-57",
+    Fencer = "Su-24",
+    Fishbed = "MiG-21",
+    Fitter = "Su-17", -- Sukhoi Su-7 and Su-17/Su-20/Su-22
+    Flogger = "MiG-23", -- and MiG-27
+    Flogger_D = "MiG-27", -- and MiG-23
+    Flagon = "Su-15",
+    Foxbat = "MiG-25",
+    Fulcrum = "MiG-29",
+    Foxhound = "MiG-31",
+    Flanker = "Su-27", -- Sukhoi Su-27/Su-30/Su-33/Su-35/Su-37 and Shenyang J-11/J-15/J-16
+    Flanker_C = "Su-30",
+    Flanker_E = "Su-35",
+    Flanker_F = "Su-37",
+    Flanker_L = "J-11A",
+    Firebird = "J-10",
+    Sea_Flanker = "Su-33",
+    Fullback = "Su-34", -- also Su-32
+    Frogfoot = "Su-25",
+    Tomcat = "F-14", -- Iran
+    Mirage = "Mirage", -- various non-NATO
+    Codling = "Yak-40",
+    Maya = "L-39",
+    -- Fighters US/NATO
+    Warthog = "A-10",
+    -- Mosquito = "A-20",
+    Skyhawk = "A-4E",
+    Viggen = "AJS37",
+    Harrier_B = "AV8BNA",
+    Harrier = "AV-8B",
+    Spirit = "B-2",
+    Aviojet = "C-101",
+    Nighthawk = "F-117A",
+    Eagle = "F-15C",
+    Mudhen = "F-15E",
+    Viper = "F-16",
+    Phantom = "F-4E",
+    Tiger = "F-5", -- was thinkg to name this MiG-25 ;)
+    Sabre = "F-86",
+    Hornet = "A-18", -- avoiding the slash
+    Hawk = "Hawk",
+    Albatros = "L-39",
+    Goshawk = "T-45",
+    Starfighter = "F-104",
+    Tornado = "Tornado",
+    -- Transport / Bomber / Others
+    Atlas = "A400",
+    Lancer = "B1-B",
+    Stratofortress = "B-52H",
+    Hercules = "C-130",
+    Super_Hercules = "Hercules",
+    Globemaster = "C-17",
+    Greyhound = "C-2A",
+    Galaxy = "C-5",
+    Hawkeye = "E-2D",
+    Sentry = "E-3A",
+    Stratotanker = "KC-135",
+    Extender = "KC-10",
+    Orion = "P-3C",
+    Viking = "S-3B",
+    Osprey = "V-22",
+    -- Bomber Rus
+    Badger = "H6-J",
+    Bear_J = "Tu-142", -- also Tu-95
+    Bear = "Tu-95", -- also Tu-142
+    Blinder = "Tu-22",
+    Blackjack = "Tu-160",
+    -- AIC / Transport / Other
+    Clank = "An-30",
+    Curl = "An-26",
+    Candid = "IL-76",
+    Midas = "IL-78",
+    Mainstay = "A-50",
+    Mainring = "KJ-2000", -- A-50 China
+    Yak = "Yak-52",
+    -- Helos
+    Helix = "Ka-27",
+    Shark = "Ka-50",
+    Hind = "Mi-24",
+    Halo = "Mi-26",
+    Hip = "Mi-8",
+    Havoc = "Mi-28",
+    Gazelle = "SA342",
+    -- Helos US
+    Huey = "UH-1H",
+    Cobra = "AH-1",
+    Apache = "AH-64",
+    Chinook = "CH-47",
+    Sea_Stallion = "CH-53",
+    Kiowa = "OH-58",
+    Seahawk = "SH-60",
+    Blackhawk = "UH-60",
+    Sea_King = "S-61",
+    -- Drones
+    UCAV = "WingLoong",
+    Reaper = "MQ-9",
+    Predator = "MQ-1A",
+
+    -- 以下是自行添加的--
+    -- basic by AIRBOSS.AircraftCarrier
+    -- @field #string RHINOE F/A-18E Superhornet (mod).
+    -- @field #string RHINOF F/A-18F Superhornet (mod).
+    -- @field #string GROWLER FEA-18G Superhornet (mod).
+    RhinoE = "FA-18E",
+    RhinoF = "FA-18F",
+    Growler = "EA-18G"
+}
+
+function HELPER.GetReportingName(TypeName)
+    local reportName = nil
+    local typename = string.lower(TypeName)
+
+    for name, value in pairs(HELPER.ReportingName) do
+        local svalue = string.lower(value)
+        if string.find(typename, svalue, 1, true) then
+            reportName = name
+        end
+    end
+    if reportName == nil then
+        env.info("HELPER: TypeName Not Found: \"" .. TypeName .. "\"")
+    end
+    return reportName
+end
+
+function HELPER.MessageToGroup(Group, MessageText, Duration, IsClear, IsSound)
+    if Group then
+        MessageText = MessageText or ""
+        Duration = Duration or 30
+        IsClear = IsClear or false
+        IsSound = IsSound or true
+
+        local groupName = Group:GetName()
+        local playersName = ""
+
+        for _, pn in ipairs(Group:GetPlayerNames()) do
+            playersName = playersName .. "/" .. pn
+        end
+        playersName = string.sub(playersName, 2)
+
+        local Text = "Group "..groupName .. " (" .. playersName .. "):\n" .. MessageText
+        env.info("Helper: Message to group:\n" .. Text)
+        MESSAGE:New(Text, Duration, nil, IsClear):ToGroup()
+
+        if IsSound then
+            USERSOUND:New(HELPER.SoundFileName):ToGroup(Group)
+        end
+    else
+        env.info("HELPER.MessageToGroup: Group is nil")
+    end
+end
--- TAC_TRAINZONE.lua
+++ TAC_TRAINZONE.lua
@@ -1,55 +1,145 @@
-TF_TRAINZONE = {
-    ClassName = "TF_TRAINZONE"
+-- TAC_TRAINZONE 类是一个用于控制飞行训练区域的Lua脚本类。
+-- 该类主要用于创建和管理一个战术训练区域，包括飞机的生成、敌人的生成和训练任务的管理。
+-- 它包含一系列参数、变量和函数，用于配置和控制训练区域的行为。
+-- 该类的主要功能是根据配置生成敌人，管理训练任务，以及处理相关事件。它可以用于模拟飞行训练和训练区域的控制。
+-- 注意: 默认的参数适用于tfm-acm模式的训练.
+--      可以通过参数调整来实现类似如BVR巡逻或其他模式的训练.
+-- 参数包括：
+--   - IsDebugMessage：是否显示调试消息。
+--   - TimerStep：计时器步长。
+--   - IsSpawnEmeny：是否生成敌人。
+--   - IsEndlessMode：是否启用无限模式，即无限生成敌人。
+--   - MaxTrainWavesFinished：最大训练波数。
+--   - IsSmartSpawn：是否启用智能生成敌人的功能。
+--   - TableEnemyTemplate：敌人模板列表。
+--   - TableSpawnAlt：生成敌人的高度列表。
+--   - TableSpawnBRA：生成敌人的方向列表。
+--   - SpawnDistance_At180：冷启动时的生成距离。
+--   - SpawnDistance_At0：热启动时的生成距离。
+--   - SpawnDistance_K：生成距离系数。
+--   - SpawnDelayMin：生成敌人的最小延迟时间。
+--   - SpawnDelayMax：生成敌人的最大延迟时间。
+--   - EnemyDestoryDelay：敌人销毁延迟时间。
+--   - EnemyEmissionOpenDelay：敌人开启雷达的延迟时间。
+--   - OutboundMaxDuration：出界最大持续时间。
+-- 变量包括：
+--   - GroupTrain：训练飞机组。
+--   - GroupEnemy：敌人飞机组。
+--   - TimerTraining：训练计时器。
+--   - TrainWavesFinished：已完成的训练波数。
+--   - SpawnTicker：生成敌人的计时器。
+--   - SpawnTickerTrigger：生成敌人的计时触发值。
+--   - OutboundTicker：出界计时器。
+-- 主要函数包括：
+--   - ShowMessage：显示消息。
+--   - New：创建一个新的训练区域。
+--   - Status：检查训练区域的状态。
+--   - SpawnEnemy：生成敌人。
+--   - OnEnterIdle：进入空闲状态时的处理。
+--   - OnEnterTraining：进入训练状态时的处理。
+--   - OnBeforeEnemySpawn：生成敌人前的处理。
+--   - OnBeforeAbortOrFinish：终止或完成训练前的处理。
+--   - OnEventHit：处理被命中事件。
+--   - AllKilled：处理全部敌人被击落事件。
+--   - OnEventCrash：处理飞机坠毁事件。
+--   - GetRandomTableElement：从列表中随机选择一个元素。
+-- 请注意：该注释提供了对类的总体了解，具体的函数实现和逻辑需要查看代码来理解。
+--
+-- The TFM_TRAINZONE class is a Lua script class used for controlling a flight training zone.
+-- This class is primarily used to create and manage a training zone, including aircraft spawning, enemy generation, and training task management.
+-- It includes a range of parameters, variables, and functions for configuring and controlling the behavior of the training zone.
+-- Parameters include:
+--   - IsDebugMessage: Whether to display debug messages.
+--   - TimerStep: Timer step.
+--   - IsSpawnEmeny: Whether to spawn enemies.
+--   - IsEndlessMode: Whether to enable endless mode, which means continuously spawning enemies.
+--   - MaxTrainWavesFinished: Maximum number of training waves.
+--   - IsSmartSpawn: Whether to enable the intelligent enemy spawning feature.
+--   - TableEnemyTemplate: List of enemy templates.
+--   - TableSpawnAlt: List of altitudes for enemy spawning.
+--   - TableSpawnBRA: List of directions for enemy spawning.
+--   - SpawnDistance_At180: Spawn distance when cold-started.
+--   - SpawnDistance_At0: Spawn distance when hot-started.
+--   - SpawnDistance_K: Spawn distance coefficient.
+--   - SpawnDelayMin: Minimum delay for enemy spawning.
+--   - SpawnDelayMax: Maximum delay for enemy spawning.
+--   - EnemyDestoryDelay: Delay for enemy destruction.
+--   - EnemyEmissionOpenDelay: Delay for enemy radar activation.
+--   - OutboundMaxDuration: Maximum duration for outbounding.
+-- Variables include:
+--   - GroupTrain: Training aircraft group.
+--   - GroupEnemy: Enemy aircraft group.
+--   - TimerTraining: Training timer.
+--   - TrainWavesFinished: Number of completed training waves.
+--   - SpawnTicker: Timer for enemy spawning.
+--   - SpawnTickerTrigger: Trigger value for enemy spawning timer.
+--   - OutboundTicker: Timer for outbounding.
+-- Key functions include:
+--   - ShowMessage: Display a message.
+--   - New: Create a new training zone.
+--   - Status: Check the status of the training zone.
+--   - SpawnEnemy: Generate enemies.
+--   - OnEnterIdle: Handling when entering the idle state.
+--   - OnEnterTraining: Handling when entering the training state.
+--   - OnBeforeEnemySpawn: Pre-handling before enemy spawning.
+--   - OnBeforeAbortOrFinish: Pre-handling before aborting or finishing training.
+--   - OnEventHit: Handle hit events.
+--   - AllKilled: Handle events when all enemies are killed.
+--   - OnEventCrash: Handle aircraft crash events.
+--   - GetRandomTableElement: Randomly select an element from a list.
+-- Please note that this comment provides a general understanding of the class, and specific function implementations and logic need to be reviewed in the code.
+TAC_TRAINZONE = {
+    ClassName = "TAC_TRAINZONE"
 }
 
 --- Parameters ---
 
-TF_TRAINZONE.IsDebugMessage = true
-TF_TRAINZONE.TimerStep = 5
+TAC_TRAINZONE.IsDebugMessage = true -- 是否显示调试消息
+TAC_TRAINZONE.TimerStep = 5 -- 计时器步长
 
--- NOTES: Default Spawn settings is for tac-acm train
-TF_TRAINZONE.IsSpawnEmeny = true
-TF_TRAINZONE.IsEndlessMode = false
-TF_TRAINZONE.MaxTrainWavesFinished = 3
+-- NOTES: Default Spawn settings is for tfm-acm train
+-- 注意: 默认的生成设置适用于tfm-acm模式的训练
 
-TF_TRAINZONE.IsSmartSpawn = true
+TAC_TRAINZONE.IsSpawnEmeny = true -- 是否生成敌人
+TAC_TRAINZONE.IsEndlessMode = false -- 是否无限模式
+TAC_TRAINZONE.MaxTrainWavesFinished = 3 -- 最大训练波数
 
--- TF_TRAINZONE.EnemyTemplateName = "EnemyTemplate"
-TF_TRAINZONE.TableEnemyTemplate = { -- "enemy-f5-highvis", --"enemy-f5-lowvis",
-"enemy-f16-highvis", "enemy-f16-lowvis","enemy-f16-lowvis-15","enemy-f16-lowvis-30"}
-TF_TRAINZONE.TableSpawnAlt = {1000, 15000, 30000} -- spawn enemy alt (meters)
-TF_TRAINZONE.TableSpawnBRA = {0, 45, 90, 120, 180}
--- spawn distance = abs(spawn_bra-180) * SpawnDistance_K + SpawnDistance_At180
-TF_TRAINZONE.SpawnDistance_At180 = 8 -- spawn distance when @cold
-TF_TRAINZONE.SpawnDistance_At0 = 20 -- spawn distance when @hot
-TF_TRAINZONE.SpawnDistance_K = (TF_TRAINZONE.SpawnDistance_At0 - TF_TRAINZONE.SpawnDistance_At180) / 180
-TF_TRAINZONE.SpawnDelayMin = 60
-TF_TRAINZONE.SpawnDelayMax = 240
+TAC_TRAINZONE.TableEnemyTemplate = {"enemy-f16-highvis", "enemy-f16-lowvis", "enemy-f16-lowvis-15",
+                                    "enemy-f16-lowvis-30"} -- 敌人模板列表
+TAC_TRAINZONE.TableSpawnAlt = {1000, 15000, 30000} -- 生成敌人的高度（米） 
+TAC_TRAINZONE.TableSpawnBRA = {0, 45, 90, 120, 180} -- 生成敌人的方向（度）
 
-TF_TRAINZONE.EnemyDestoryDelay = 30
-TF_TRAINZONE.EnemyEmissionOpenDelay = 30
+TAC_TRAINZONE.IsSmartSpawn = true -- 是否智能生成(根据迎击角度改变生成距离)
+TAC_TRAINZONE.SpawnDistance_At180 = 8 -- 冷启动时的生成距离
+TAC_TRAINZONE.SpawnDistance_At0 = 20 -- 热启动时的生成距离
+TAC_TRAINZONE.SpawnDistance_K = 0 -- 生成距离系数(标定无用,函数内重新计算)
+TAC_TRAINZONE.SpawnDelayMin = 60 -- 生成敌人的最小延迟时间（秒）
+TAC_TRAINZONE.SpawnDelayMax = 240 -- 生成敌人的最大延迟时间（秒）
 
-TF_TRAINZONE.OutboundMaxDuration = 90
+TAC_TRAINZONE.EnemyDestoryDelay = 30 -- 敌人销毁延迟时间（秒）
+TAC_TRAINZONE.EnemyEmissionOpenDelay = 30 -- 敌人开启雷达延迟时间（秒）
+
+TAC_TRAINZONE.OutboundMaxDuration = 90 -- 出界最大持续时间（秒）
 
 ---  Parameters End---
 
 --- values ---
 
-TF_TRAINZONE.GroupTrain = nil
-TF_TRAINZONE.GroupEnemy = nil
+TAC_TRAINZONE.GroupTrain = nil
+TAC_TRAINZONE.GroupEnemy = nil
 
-TF_TRAINZONE.TimerTraining = nil
-TF_TRAINZONE.TrainWavesFinished = nil
+TAC_TRAINZONE.TimerTraining = nil
+TAC_TRAINZONE.TrainWavesFinished = nil
 
-TF_TRAINZONE.SpawnTicker = 0
-TF_TRAINZONE.SpawnTickerTrigger = 0
+TAC_TRAINZONE.SpawnTicker = 0
+TAC_TRAINZONE.SpawnTickerTrigger = 0
 
-TF_TRAINZONE.OutboundTicker = 0
+TAC_TRAINZONE.OutboundTicker = 0
 
 --- values end ---
 
 --- functions ---
-function TF_TRAINZONE:ShowMessage(Message, Duration, IsClean, IsToAll, ToGroup)
+function TAC_TRAINZONE:ShowMessage(Message, Duration, IsClean, IsToAll, ToGroup)
 
     Message = Message or ""
     IsClean = IsClean or false
@@ -69,7 +159,7 @@ function TF_TRAINZONE:ShowMessage(Message, Duration, IsClean, IsToAll, ToGroup)
     end
 end
 
-function TF_TRAINZONE:New(ZoneName)
+function TAC_TRAINZONE:New(ZoneName)
     local self = BASE:Inherit(self, FSM:New())
 
     self.ZoneTraining = ZONE:New(ZoneName)
@@ -101,7 +191,7 @@ function TF_TRAINZONE:New(ZoneName)
     return self
 end
 
-function TF_TRAINZONE:Status()
+function TAC_TRAINZONE:Status()
 
     if self:Is("Idle") then
         local groups = SET_GROUP:New():FilterCoalitions("blue"):FilterCategoryAirplane()
@@ -170,7 +260,8 @@ function TF_TRAINZONE:Status()
         end
     end
 
-    -- -- enemy check
+    -- bug:妈的 isalive一直是false, 黑人问号脸, 弃用
+    -- -- enemy check    
     -- if self:Is("ThreatSpawned") then
     --     if self.GroupEnemy then
     --         if not self.GroupEnemy:IsAlive() then
@@ -200,12 +291,13 @@ function TF_TRAINZONE:Status()
     return self
 end
 
-function TF_TRAINZONE:SpawnEnemy()
+function TAC_TRAINZONE:SpawnEnemy()
     -- ref
     -- https://flightcontrol-master.github.io/MOOSE_DOCS/Documentation/Wrapper.Group.html
     -- https://flightcontrol-master.github.io/MOOSE_DOCS/Documentation/Core.Spawn.html    
 
     -- do spawm enemy parameters
+    -- bug:无法通过alt_spawn控制生成飞机的高度,只能通过飞机模板来控制
     local bra_degree = 0
     local distance_spawn = 8
     local alt_spawn = GetRandomTableElement(self.TableSpawnAlt)
@@ -216,7 +308,8 @@ function TF_TRAINZONE:SpawnEnemy()
         if math.random(2) == 1 then
             local bra_degree = 360 - bra_degree
         end
-        distance_spawn = self.SpawnDistance_At180 + math.abs(bra_degree - 180) * self.SpawnDistance_K
+        local k_distance = (self.SpawnDistance_At0 - self.SpawnDistance_At180) / 180
+        distance_spawn = self.SpawnDistance_At180 + math.abs(bra_degree - 180) * k_distance
         local zoneSpawn = ZONE_UNIT:New("spawn", self.GroupTrain:GetUnits()[1], 10, {
             rho = distance_spawn * 1800,
             theta = bra_degree,
@@ -260,14 +353,14 @@ function TF_TRAINZONE:SpawnEnemy()
     -- self.GroupEnemy:PushTask()
 end
 
-function TF_TRAINZONE:OnEnterIdle(From, Event, To)
+function TAC_TRAINZONE:OnEnterIdle(From, Event, To)
     self:ShowMessage("导演部: " .. self.ZoneTraining:GetName() .. " 净空, 可以进入机组训练", nil, false,
         true)
     self.ZoneTraining:DrawZone(-1, {0, 1, 0}, 0.8, nil, nil, 1, false)
     return true
 end
 
-function TF_TRAINZONE:OnEnterTraining(From, Event, To)
+function TAC_TRAINZONE:OnEnterTraining(From, Event, To)
 
     self.ZoneTraining:DrawZone(-1, {1, 0, 0}, 0.8, nil, nil, 1, false)
 
@@ -293,14 +386,14 @@ function TF_TRAINZONE:OnEnterTraining(From, Event, To)
     return true
 end
 
-function TF_TRAINZONE:OnBeforeEnemySpawn(From, Event, To)
+function TAC_TRAINZONE:OnBeforeEnemySpawn(From, Event, To)
 
     self:SpawnEnemy()
 
     return true
 end
 
-function TF_TRAINZONE:OnBeforeAbortOrFinish(From, Event, To)
+function TAC_TRAINZONE:OnBeforeAbortOrFinish(From, Event, To)
     self:ShowMessage("退出训练空域以重置训练, 或直接返回机场")
 
     if self.GroupEnemy then
@@ -316,7 +409,7 @@ function TF_TRAINZONE:OnBeforeAbortOrFinish(From, Event, To)
     return true
 end
 
-function TF_TRAINZONE:OnEventHit(EventData)
+function TAC_TRAINZONE:OnEventHit(EventData)
     if self:Is("Training") or self:Is("ThreatSpawned") then
         if self.GroupTrain then
             local group = EventData.TgtGroup
@@ -328,7 +421,7 @@ function TF_TRAINZONE:OnEventHit(EventData)
     end
 end
 
-function TF_TRAINZONE:AllKilled()
+function TAC_TRAINZONE:AllKilled()
     self:ShowMessage("GoodKill~ GoodKill~")
     self.TrainWavesFinished = self.TrainWavesFinished + 1
     if self.IsEndlessMode or self.TrainWavesFinished < self.MaxTrainWavesFinished then
@@ -336,12 +429,12 @@ function TF_TRAINZONE:AllKilled()
         self.GroupEnemy = nil
         self:EnemyDisarm()
     elseif self.TrainWavesFinished >= self.MaxTrainWavesFinished then
-        self:ShowMessage("你机队已经完成训练.")
+        self:ShowMessage("导演部: 恭喜,你机队已经完成训练.")
         self:AbortOrFinish()
     end
 end
 
-function TF_TRAINZONE:OnEventCrash(EventData)
+function TAC_TRAINZONE:OnEventCrash(EventData)
     env.info("==CRASH EVENT==")
     env.info("ini group:" .. EventData.IniGroup.GroupName)
     env.info("tgt group:" .. EventData.TgtGroup.GroupName)
